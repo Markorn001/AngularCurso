0c712a71e0666ead3da5b6f967316543d21c20ac
marco <marcoramos687@gmail.com>
2022-10-19 01:18:48 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+ // template: '<span>Fernando</span>',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  
 })
+
 export class AppComponent {
-  title = 'bases';
+  titulo: string = 'Contador App';
+  numero: number = 0;
+  base: number= 5;
+
+  acumular(valor: number){
+    this.numero+=valor;
+
+  }
+
+  sumar(){
+    this.numero+=1000000000000000000;
+  }
+
+  restar(){
+    this.numero-=1000000000000000000;
+  }
 }
